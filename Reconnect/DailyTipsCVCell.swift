be06be61da910783e8d8f1c7f34f834d06f9a910
yolanda halim <yolan9091@gmail.com>
2019-02-11 16:28:36 +0700

    Added home storyboard

---
+++ Reconnect/DailyTipsCVCell.swift
@@ -0,0 +1,11 @@
+//
+//  DailyTipsCVCell.swift
+//  Reconnect
+//
+//  Created by Yolanda Halim on 08/02/19.
+//  Copyright © 2019 Codebusters. All rights reserved.
+//
+
+import UIKit
+
+class DailyTipsCVCell: UICollectionViewCell {}
