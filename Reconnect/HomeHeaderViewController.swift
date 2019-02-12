8906fbef8f27d3046f997b6cd5e86bfe1006e384
yolanda halim <yolan9091@gmail.com>
2019-02-12 07:53:34 +0700

    Changes for Home table view

--- Reconnect/HomeHeaderViewController.swift
+++ Reconnect/HomeHeaderViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class HomeHeaderViewController: UIViewController {
-
+class HomeHeaderViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var dailyContentCV: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        dailyContentCV.isPagingEnabled = true
+        dailyContentCV.delegate = self
+        dailyContentCV.dataSource = self
+        
         testLabel.text = "qwer ty qwer tyqwer tyq wertyq wertyqwe rtyqwer tyqwert yqwerty qwerty"
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if let flowLayout = self.dailyContentCV.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.itemSize = CGSize(width:
+                self.dailyContentCV.bounds.width,
+                                         height: self.dailyContentCV.bounds.height)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        print(indexPath)
+        if indexPath.section == 0 {
+            if let dailyQuoteCVCell = collectionView.dequeueReusableCell(withReuseIdentifier: "dailyQuoteCell", for: indexPath) as? DailyQuoteCVCell {
+                dailyQuoteCVCell.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+                return dailyQuoteCVCell
+            }
+        }
+        
+        let dailyTipsCVCell = collectionView.dequeueReusableCell(withReuseIdentifier: "dailyTipsCell", for: indexPath) as? DailyTipsCVCell
+        dailyTipsCVCell?.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        return dailyTipsCVCell ?? UICollectionViewCell()
+    }
     /*
     // MARK: - Navigation
 
--- Reconnect/HomeViewController.swift
+++ Reconnect/HomeViewController.swift
@@ -8,78 +8,24 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    @IBOutlet weak var dailyQuoteButton: UIButton!
-    @IBOutlet weak var dailyTipsButton: UIButton!
-    @IBOutlet weak var dailyContentCV: UICollectionView!
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var homeTableView: UITableView!
+    @IBOutlet weak var dailyContentTableHeader: UIView!
+    private let needToAddTagContacts : [Person] = []
+   
     override func viewDidLoad() {
-        super.viewDidLoad()        // Do any additional setup after loading the view.
-        dailyContentCV.isPagingEnabled = true
-        dailyContentCV.delegate = self
-        dailyContentCV.dataSource = self
-        
-//        self.dailyContentCV!.register(DailyQuoteCVCell.self, forCellWithReuseIdentifier: "dailyQuoteCell")
-//        self.dailyContentCV!.register(DailyTipsCVCell.self, forCellWithReuseIdentifier: "dailyQuoteCell")
+        super.viewDidLoad()
+        dailyContentTableHeader.frame(forAlignmentRect: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: homeTableView.visibleSize.width, height: homeTableView.visibleSize.height * 0.4)))
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
     }
 
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        if let flowLayout = self.dailyContentCV.collectionViewLayout as? UICollectionViewFlowLayout {
-            flowLayout.itemSize = CGSize(width: self.dailyContentCV.bounds.width,
-                                        height: self.dailyContentCV.bounds.height)
-        }
-    }
-
-    @IBAction func changeToDailyQuote(_ sender: Any) {
-        dailyQuoteButton.setTitleColor(.black, for: .normal)
-        dailyTipsButton.setTitleColor(.gray, for: .normal)
-//        dailyContentCV.beginInteractiveMovementForItem(at: IndexPath.init(item: 0, section: 0))
-//        dailyContentCV.updateInteractiveMovementTargetPosition(CGPoint(x: 0, y: 0))
-//        dailyContentCV.endInteractiveMovement()
-    }
-
-    @IBAction func changeToDailyTips(_ sender: Any) {
-//        dailyTipsButton.setTitleColor(.black, for: .normal)
-//        dailyQuoteButton.setTitleColor(.gray, for: .normal)
-//        print("sections:  \(dailyContentCV.numberOfSections)")
-//        print("item in section 0:  \(dailyContentCV.numberOfItems(inSection: 0))")
-        dailyContentCV.selectItem(at: dailyContentCV.indexPathsForVisibleItems[0],
-                                  animated: true, scrollPosition: UICollectionView.ScrollPosition.left)
-//       print("item in section 1:  \(dailyContentCV.numberOfItems(inSection: 1))")
-//        print("Index path for item:  \(dailyContentCV.indexPathForItem(at: CGPoint(x: 0, y: 0)) as Any)")
-//                dailyContentCV.beginInteractiveMovementForItem(at: IndexPath.init(item: 1, section: 0))
-//        dailyContentCV.updateInteractiveMovementTargetPosition(CGPoint(x: 0, y: 0))
-//        dailyContentCV.endInteractiveMovement()
-    }
-
-    // delegate
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.item + 1)
-    }
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        print(indexPath)
-        if indexPath.section == 0 {
-            if let dailyQuoteCVCell = collectionView.dequeueReusableCell(withReuseIdentifier: "dailyQuoteCell", for: indexPath) as? DailyQuoteCVCell {
-                dailyQuoteCVCell.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-                return dailyQuoteCVCell
-            }
-        }
-        
-        let dailyTipsCVCell = collectionView.dequeueReusableCell(withReuseIdentifier: "dailyTipsCell", for: indexPath) as? DailyTipsCVCell
-        dailyTipsCVCell?.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
-        return dailyTipsCVCell ?? UICollectionViewCell()
-        
-    }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {  return UITableViewCell()    }
     /*
     // MARK: - Navigation
 
